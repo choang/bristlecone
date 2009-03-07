813fcb3055e005b6e35f675dec85d713d1c8a5ca
rhodges <rhodges@112a3636-d557-07a8-cdcc-700a6d9a54d0>
2009-03-07 06:36:01 +0000

    BRI-18 Implemented TableHelper class which is superclass of TableSetHelper.  Added unit test cases for same.

--- bristlecone/src/com/continuent/bristlecone/benchmark/db/TableSetHelper.java
+++ bristlecone/src/com/continuent/bristlecone/benchmark/db/TableSetHelper.java
@@ -35,19 +35,14 @@ import org.apache.log4j.Logger;
 import com.continuent.bristlecone.benchmark.BenchmarkException;
 
 /**
- * Implements methods to create, populate, and drop tables containing
+ * Implements methods to create, populate, and drop sets of tables containing
  * test data.  
  * 
  * @author rhodges
  */
-public class TableSetHelper
+public class TableSetHelper extends TableHelper
 {
   private static Logger logger = Logger.getLogger(TableSetHelper.class);
-
-  private final String connectionUrl; 
-  private final String login;
-  private final String password; 
-  private final SqlDialect sqlDialect;
   
   /** 
    * Creates a new instance. 
@@ -60,54 +55,9 @@ public class TableSetHelper
    */
   public TableSetHelper(String url, String login, String password)
   {
-    this.connectionUrl = url;
-    this.login = login;
-    this.password = password;
-    this.sqlDialect = SqlDialectFactory.getInstance().getDialect(url);
-    loadDriver(sqlDialect.getDriver());
+    super(url, login, password);
   }
   
-  /**
-   * Loads a JDBC driver. 
-   */
-  public void loadDriver(String name) throws BenchmarkException
-  {
-    try
-    {
-      Class.forName(name);
-    }
-    catch (Exception e)
-    {
-      throw new BenchmarkException("Unable to load JDBC driver: " + name, e);
-    }
-  }
-  
-  /** 
-   * Returns the SQLDialect used by this helper. 
-   */
-  public SqlDialect getSqlDialect()
-  {
-    return sqlDialect;
-  }
-
-  /**
-   * Runs an arbitrary SQL command with proper clean-up of resources.  
-   */
-  public void execute(String sql) throws SQLException
-  {
-    Connection conn = getConnection();
-    Statement stmt = conn.createStatement();
-    try
-    {
-      stmt.execute(sql);
-    }
-    finally
-    {
-      releaseStatement(stmt);
-      releaseConnection(conn);
-    }
-  }
-
   /** 
    * Creates all tables in a table set.  Any existing tables are dropped. 
    */
@@ -118,41 +68,11 @@ public class TableSetHelper
     
     int count = tableSet.getCount();
     Table[] tables = tableSet.getTables();
-    Connection conn = getConnection();
    
     for (int i = 0; i < count; i++)
     {
-      String createSql = null;
-      Statement stmt = conn.createStatement();
-      try
-      {
-        // Create the table. 
-        createSql = sqlDialect.getCreateTable(tables[i]);
-        stmt.execute(createSql);
-        
-        // Add extra index for any indexed columns. 
-        for (int c = 0; c < tables[i].getColumns().length; c++)
-        {
-          Column col = tables[i].getColumns()[c];
-          if (col.isIndexed())
-          {
-            createSql = sqlDialect.getCreateIndex(tables[i], col);
-            stmt.execute(createSql);
-          }
-        }
-      }
-      catch (SQLException e)
-      {
-        logger.warn("Table creation failed: " + createSql, e);
-        releaseConnection(conn);
-        throw e;
-      }
-      finally
-      {
-        releaseStatement(stmt);
-      }
+      create(tables[i], true);
     }
-    releaseConnection(conn);
   }
 
   /** Populates tables in the TableSet with random data. */
@@ -218,146 +138,10 @@ public class TableSetHelper
   {
     int count = tableSet.getCount();
     Table[] tables = tableSet.getTables();
-    Connection conn = getConnection();
     
     for (int i = 0; i < count; i++)
     {
-      String dropSql = sqlDialect.getDropTable(tables[i]);
-      
-      Statement stmt = conn.createStatement();
-      try 
-      {
-        stmt.execute(dropSql);
-      }
-      catch (SQLException e) 
-      {
-        if (ignore)
-          logger.debug("Table deletion failure ignored: " + dropSql);
-        else
-        {
-          logger.warn("Table drop failed: " + dropSql, e);
-          releaseConnection(conn);
-          throw e;
-        }
-      }
-      finally
-      {
-        releaseStatement(stmt);
-      }
+      drop(tables[i], ignore);
     }
-    releaseConnection(conn);
-  }
-
-  /** Gets a database connection. */
-  public Connection getConnection()
-      throws SQLException
-  {
-    // Connect to database.
-    logger.debug("Connecting to database: url=" + connectionUrl + " user=" + login);
-    Connection conn = DriverManager.getConnection(connectionUrl, login, password);
-    logger.debug("Obtained database connection: " + conn);
-    return conn; 
-  }
-  
-  /** Releases a database connection. */
-  public void releaseConnection(Connection conn)
-  {
-    // Connect to database.
-    logger.debug("Releasing database connection: " + conn);
-    try
-    {
-      conn.close();
-    }
-    catch (SQLException e)
-    {
-      logger.debug("Connection release failed", e);
-    }
-  }
-  
-  /** Releases a statement. */
-  public void releaseStatement(Statement stmt)
-  {
-    // Connect to database.
-    logger.debug("Releasing database statement: " + stmt);
-    try
-    {
-      stmt.close();
-    }
-    catch (SQLException e)
-    {
-      logger.debug("Statement release failed", e);
-    }
-  }
-  
-  /**
-   * Confirm [non-]existence of a particular row indexed by a key.
-   *
-   * @param key   Key value
-   * @param exists If true, expect the key to exist.  Otherwise we
-   *               expect not to find it.
-   * @param limit  Number of milliseconds to wait before giving up.
-   * @param logInterval  Interval in milliseconds between writing messages.
-   * @return true if the test succeeded, false if we exceeded the limit
-   * 
-   * @throws BenchmarkException If the test criteria appear to be bad
-   * @throws Execption If there is any other exception
-   */
-  public boolean testRowExistence(PreparedStatement keyQuery, 
-      String key, boolean exists, long limit, long logInterval) 
-      throws BenchmarkException, Exception
-  {
-    long limitTimer = System.currentTimeMillis();
-    long logIntervalTimer = limitTimer;
-    keyQuery.setString(1, key);
-
-    // Repeat the search until we exceed the time limit.
-    do
-    {
-      // Look for matching tables.
-      ResultSet rs = null;
-      int matches = 0;
-      try {
-        rs = keyQuery.executeQuery();
-        while (rs.next())
-        {
-          matches++;
-        }
-      }
-      finally
-      {
-        if (rs != null)
-          rs.close();
-      }
-
-      // If there are multiple matches the test selection criteria are buggy.
-      if (matches > 1)
-        throw new BenchmarkException("Found multiple matches when searching for record: key="
-            + key);
-
-      // Apply result matching to ensure we get what we are looking for...
-      if (exists && matches == 1)
-      {
-        // Key expected to exist and we found it.
-        return true;
-      }
-      else if (!exists && matches == 0)
-      {
-        // Key not expected to exist and we did not find it.
-        return true;
-      }
-
-      // See if we need to log a message because we have been waiting over the
-      // log limit.
-      if ((System.currentTimeMillis() - logIntervalTimer) > logInterval)
-      {
-        logIntervalTimer = System.currentTimeMillis();
-        logger.info("Waited " + (logInterval / 1000) +
-            " to test row existence: key=" + key + " existence=" + exists);
-      }
-    }
-    while ((System.currentTimeMillis() - limitTimer) < limit);
-
-    // If we got this far the test exceeded the timeout limit and is a failure.
-    return false;
   }
 }
--- bristlecone/test/com/continuent/bristlecone/benchmark/test/DbUtilitiesTest.java
+++ bristlecone/test/com/continuent/bristlecone/benchmark/test/DbUtilitiesTest.java
@@ -39,47 +39,48 @@ import com.continuent.bristlecone.benchmark.db.Column;
 import com.continuent.bristlecone.benchmark.db.SqlDialect;
 import com.continuent.bristlecone.benchmark.db.SqlDialectFactory;
 import com.continuent.bristlecone.benchmark.db.Table;
+import com.continuent.bristlecone.benchmark.db.TableHelper;
 import com.continuent.bristlecone.benchmark.db.TableSet;
 import com.continuent.bristlecone.benchmark.db.TableSetHelper;
 
 /**
- * Implements a unit test designed to check database utility functions, 
- * specifically, creating, loading with data, and deleting tables using
- * TableSet definitions.  
+ * Implements a unit test designed to check database utility functions,
+ * specifically, creating, loading with data, and deleting tables using TableSet
+ * definitions.
  * 
  * @author rhodges
- *
  */
 public class DbUtilitiesTest extends TestCase
 {
   String driver;
-  String url; 
+  String url;
   String login;
   String password;
-  
+
   // Does nothing for now
   protected void setUp() throws Exception
   {
-      // Set test.properties file name.
-      String testPropertiesName = System.getProperty("test.properties");
-      if (testPropertiesName == null)
-          testPropertiesName = "test.properties";
+    // Set test.properties file name.
+    String testPropertiesName = System.getProperty("test.properties");
+    if (testPropertiesName == null)
+      testPropertiesName = "test.properties";
 
-      // Load properties file.
-      Properties props = new Properties();
-      File f = new File(testPropertiesName);
-      if (f.canRead())
-      {
-          FileInputStream fis = new FileInputStream(f);
-          props.load(fis);
-          fis.close();
-      }
+    // Load properties file.
+    Properties props = new Properties();
+    File f = new File(testPropertiesName);
+    if (f.canRead())
+    {
+      FileInputStream fis = new FileInputStream(f);
+      props.load(fis);
+      fis.close();
+    }
 
-      // Set values used for test.
-      driver = props.getProperty("driver", "org.hsqldb.jdbcDriver");
-      url = props.getProperty("url", "jdbc:hsqldb:file:build/testdb/testdb;shutdown=true");
-      login = props.getProperty("login", "sa");
-      password = props.getProperty("password", null);
+    // Set values used for test.
+    driver = props.getProperty("driver", "org.hsqldb.jdbcDriver");
+    url = props.getProperty("url",
+        "jdbc:hsqldb:file:build/testdb/testdb;shutdown=true");
+    login = props.getProperty("login", "sa");
+    password = props.getProperty("password", null);
   }
 
   protected void tearDown() throws Exception
@@ -87,8 +88,8 @@ public class DbUtilitiesTest extends TestCase
     super.tearDown();
   }
 
-  /** 
-   * Instantiate a column and confirm that values are correctly returned. 
+  /**
+   * Instantiate a column and confirm that values are correctly returned.
    */
   public void testColumn()
   {
@@ -102,39 +103,39 @@ public class DbUtilitiesTest extends TestCase
   }
 
   /**
-   * Generate a table and check that values are correctly returned. 
+   * Generate a table and check that values are correctly returned.
    */
   public void testTable()
   {
     Table t = allTypesTable("testTable1");
     assertNotNull(t);
     assertEquals("Checking table name", "testTable1", t.getName());
-    
-    Column key = t.getPrimaryKey(); 
+
+    Column key = t.getPrimaryKey();
     assertNotNull("Checking that key is not null", key);
     assertEquals("Checking key name", "t_integer", key.getName());
-    
+
     Column[] cols = t.getColumns();
     assertNotNull("Checking columns", cols);
     assertEquals("Checking column number", 8, cols.length);
   }
-  
+
   /**
-   * Generate a table with all types and confirm that all non-prepared 
-   * statements work properly. 
+   * Generate a table with all types and confirm that all non-prepared
+   * statements work properly.
    */
   public void testSqlDialect1() throws Exception
   {
-    // Get data for test. 
+    // Get data for test.
     Table t = allTypesTable("testSqlDialect");
     SqlDialect dialect = SqlDialectFactory.getInstance().getDialect(url);
     Connection conn = getConnection();
-    
+
     assertNotNull("Checking table", t);
-    assertNotNull("Checking dialect", dialect); 
+    assertNotNull("Checking dialect", dialect);
     assertNotNull("Checking connection", conn);
 
-    // Drop table in case it already exists 
+    // Drop table in case it already exists
     Statement stmt = null;
     try
     {
@@ -152,44 +153,44 @@ public class DbUtilitiesTest extends TestCase
         stmt = null;
       }
     }
-    
-    // Test table creation.   
+
+    // Test table creation.
     stmt = conn.createStatement();
     String createTable = dialect.getCreateTable(t);
     stmt.execute(createTable);
-    
-    // Select from the table. 
+
+    // Select from the table.
     String selectAll = dialect.getSelectAll(t);
     stmt.execute(selectAll);
-    
-    // Delete everything from the table. 
+
+    // Delete everything from the table.
     String deleteAll = dialect.getDeleteAll(t);
     stmt.execute(deleteAll);
-    
-    // Drop the table. 
+
+    // Drop the table.
     String deleteTable = dialect.getDropTable(t);
     stmt.execute(deleteTable);
-    
-    // Clean up. 
+
+    // Clean up.
     stmt.close();
     conn.close();
   }
-    
+
   /**
-   * Generate a table and confirm that cross product selects work properly.   
+   * Generate a table and confirm that cross product selects work properly.
    */
   public void testSqlDialect2() throws Exception
   {
-    // Get data for test. 
+    // Get data for test.
     Table t = simpleTable("testSqlDialect2");
     SqlDialect dialect = SqlDialectFactory.getInstance().getDialect(url);
     Connection conn = getConnection();
-    
+
     assertNotNull("Checking table", t);
-    assertNotNull("Checking dialect", dialect); 
+    assertNotNull("Checking dialect", dialect);
     assertNotNull("Checking connection", conn);
 
-    // Drop table in case it already exists 
+    // Drop table in case it already exists
     Statement stmt = null;
     try
     {
@@ -207,31 +208,31 @@ public class DbUtilitiesTest extends TestCase
         stmt = null;
       }
     }
-    
-    // Test table creation.   
+
+    // Test table creation.
     stmt = conn.createStatement();
     String createTable = dialect.getCreateTable(t);
     stmt.execute(createTable);
-    
-    // Test index generation. 
+
+    // Test index generation.
     String createIndex = dialect.getCreateIndex(t, t.getColumn("t_varchar"));
     stmt.execute(createIndex);
-    
-    // Test table insert. 
+
+    // Test table insert.
     String insert = dialect.getInsert(t);
     PreparedStatement pstmt = conn.prepareStatement(insert);
     pstmt.setInt(1, 1);
     pstmt.setString(2, "data");
     pstmt.execute();
-    
-    // Test table update. 
+
+    // Test table update.
     String update = dialect.getUpdateByKey(t);
     pstmt = conn.prepareStatement(update);
     pstmt.setString(1, "data2");
     pstmt.setInt(2, 1);
     pstmt.execute();
-    
-    // Test select. 
+
+    // Test select.
     String select = dialect.getSelectByKey(t);
     pstmt = conn.prepareStatement(select);
     pstmt.setInt(1, 1);
@@ -247,95 +248,188 @@ public class DbUtilitiesTest extends TestCase
       }
     }
     rs.close();
-    
+
     assertEquals("Row count should be one", 1, count);
     assertEquals("Found the expected value from update", true, ok);
-    
-    // Select rows from the table using a select cross product. 
+
+    // Select rows from the table using a select cross product.
     String selectCrossProduct = dialect.getSelectCrossProduct(t);
     rs = stmt.executeQuery(selectCrossProduct);
     rs.close();
-    
-    // Select count(*) from the table using a select cross product. 
+
+    // Select count(*) from the table using a select cross product.
     String selectCrossProductCount = dialect.getSelectCrossProductCount(t);
     rs = stmt.executeQuery(selectCrossProductCount);
     rs.close();
-    
-    // Drop the table. 
+
+    // Drop the table.
     String deleteTable = dialect.getDropTable(t);
     stmt.execute(deleteTable);
-    
-    // Clean up. 
+
+    // Clean up.
     pstmt.close();
     stmt.close();
     conn.close();
   }
 
   /**
-   * Shows that we can create and populate tables for a TableSet.  
+   * Shows that we can create and populate tables for a TableSet.
    */
   public void testDataGeneration1() throws Exception
   {
-    // Get data for test. 
+    // Get data for test.
     TableSet ts = allTypesTableSet("testDG1_", 10, 100);
     TableSetHelper tsHelper = new TableSetHelper(url, login, password);
-    
+
     assertNotNull("Checking table", ts);
 
-    // Create new tables. 
+    // Create new tables.
     tsHelper.createAll(ts);
-    
-    // Populate data. 
+
+    // Populate data.
     tsHelper.populateAll(ts);
-    
-    // Drop tables and go home. 
+
+    // Drop tables and go home.
     tsHelper.dropAll(ts, false);
   }
-    
-  // Create column defintions for all supported types. 
-  private Column[] allTypes()
+
+  /**
+   * Shows that a table helper can create and drop a table with various data
+   * types.  All combinations of add/drop are verified. 
+   */
+  public void testTableHelper1() throws Exception
   {
-    Column[] cols = new Column[] {
-        new Column("t_integer", Types.INTEGER, 0, 0, true, false), 
-        new Column("t_blob", Types.BLOB, 100), 
-        new Column("t_char", Types.CHAR, 10), 
-        new Column("t_clob", Types.CLOB, 100), 
-        new Column("t_double", Types.DOUBLE), 
+    Table table = allTypesTable("testTH1");
+    TableHelper helper = new TableHelper(url, login, password);
+    assertNotNull("Checking table", table);
+    
+    // Create and drop table.  Should work no matter what. 
+    helper.create(table, true);
+    
+    // Create it again.  This works because we drop first. 
+    helper.create(table, true);
+    
+    // This should fail because the table already exists. 
+    try
+    {
+      helper.create(table, false);
+      throw new Exception("Able to add table when it already exists: " + table.getName());
+    }
+    catch (SQLException e)
+    {
+    }
+    
+    // Drop the table.  This should work. 
+    helper.drop(table, false);
+    
+    // This works because we ignore errors from dropping a non-existent table.
+    helper.drop(table, true);
+    
+    // This works because the table is deleted. 
+    helper.create(table, false);
+  }
+
+  /**
+   * Shows that a table helper can insert and delete rows. 
+   */
+  public void testTableHelper2() throws Exception
+  {
+    Column[] cols = new Column[]{
+        new Column("t_key", Types.INTEGER, 0, 0, true, false),
+        new Column("t_char", Types.CHAR, 10),
+        new Column("t_double", Types.DOUBLE),
         new Column("t_float", Types.FLOAT),
         new Column("t_smallint", Types.SMALLINT),
         new Column("t_varchar", Types.VARCHAR, 10)
     };
+
+    Table table = new Table("testTH2", cols);
+    TableHelper helper = new TableHelper(url, login, password);
+    assertNotNull("Checking table", table);
+    
+    // Create the table. 
+    helper.create(table, true);
+
+    // Insert a bunch of rows.
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+      try
+      {
+        Object[] values = new Object[6];
+        values[0] = i;
+        values[1] = new Integer(i).toString();
+        values[2] = (double) i;
+        values[3] = (float) i;
+        values[4] = i;
+        values[5] = new Integer(i).toString();
+        helper.insert(table, values);
+      }
+      catch (Exception e)
+      {
+        throw new Exception("Insert failed on row " + i, e);
+      }
+    }
+      
+    // Delete rows.
+    for (i = 0; i < 5; i++)
+    {
+      try
+      {
+        Object[] values = new Object[1];
+        values[0] = i;
+        helper.delete(table, values);
+      }
+      catch (Exception e)
+      {
+        throw new Exception("Delete failed on row " + i, e);
+      }
+    }
+  }
+
+  // Create column definitions for all supported types.
+  private Column[] allTypes()
+  {
+    Column[] cols = new Column[]{
+        new Column("t_integer", Types.INTEGER, 0, 0, true, false),
+        new Column("t_blob", Types.BLOB, 100),
+        new Column("t_char", Types.CHAR, 10),
+        new Column("t_clob", Types.CLOB, 100),
+        new Column("t_double", Types.DOUBLE),
+        new Column("t_float", Types.FLOAT),
+        new Column("t_smallint", Types.SMALLINT),
+        new Column("t_varchar", Types.VARCHAR, 10)};
     return cols;
   }
 
-  // Create table definition containing an integer key and a varchar column.  
+  // Create table definition containing an integer key and a varchar column.
   private Table simpleTable(String name)
   {
-    Column[] cols = new Column[2]; 
-    cols[0] = new Column("t_integer", Types.INTEGER, 0, 0, true, false); 
+    Column[] cols = new Column[2];
+    cols[0] = new Column("t_integer", Types.INTEGER, 0, 0, true, false);
     cols[1] = new Column("t_varchar", Types.VARCHAR, 10);
     cols[1].setIndexed(true);
 
     Table t = new Table(name, cols);
     return t;
   }
-  
-  // Create table definition containing all supported types.  
+
+  // Create table definition containing all supported types.
   private Table allTypesTable(String name)
   {
     Column[] cols = allTypes();
     Table t = new Table(name, cols);
     return t;
   }
-  
+
   // Create table set definition containing all supported types.
   private TableSet allTypesTableSet(String prefix, int count, int rows)
   {
     Column[] cols = allTypes();
     return new TableSet(prefix, count, rows, cols);
   }
-  
-  // Returns a JDBC connection instance. 
+
+  // Returns a JDBC connection instance.
   private Connection getConnection() throws Exception
   {
     Connection c;
@@ -344,4 +438,3 @@ public class DbUtilitiesTest extends TestCase
     return c;
   }
 }
-
