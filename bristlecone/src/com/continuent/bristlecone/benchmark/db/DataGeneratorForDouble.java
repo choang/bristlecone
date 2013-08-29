570e7f08043e72db26ebb1cbe026dec13e2d3ecf
linas.virbalas <linas.virbalas@continuent.com>
2013-08-29 14:56:20 +0000

    REP-132
    Fixing double generator random distribution to generate not only astronomical numbers, but also numbers of various magnitude and with decimals.

--- bristlecone/src/com/continuent/bristlecone/benchmark/db/DataGeneratorForDouble.java
+++ bristlecone/src/com/continuent/bristlecone/benchmark/db/DataGeneratorForDouble.java
@@ -17,32 +17,45 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  *
  * Initial developer(s): Robert Hodges and Ralph Hannus.
- * Contributor(s):
+ * Contributor(s): Linas Virbalas
  */
 
 package com.continuent.bristlecone.benchmark.db;
 
 /**
- * Generates double values. 
+ * Generates double values.
  * 
  * @author rhodges
- *
  */
 public class DataGeneratorForDouble implements DataGenerator
 {
-  private static final double max = Float.MAX_VALUE;
-  //max from float so that this works better with oracle
- 
+  private static final boolean astronomicalNumbersOnly = false;
+
   /** Create a new instance. */
   DataGeneratorForDouble()
   {
   }
-  
+
   /** Generate next value up to the boundary value. */
   public Object generate()
   {
+    double max = 0;
+    if (astronomicalNumbersOnly)
+    {
+      // Max from float so that this works better with oracle.
+      max = Float.MAX_VALUE;
+    }
+    else
+    {
+      // REP-132 - must use different magnitude each time, otherwise we
+      // generate only astronomical numbers.
+      double magnitude = (int) (38 * Math.random());
+      // 3.4028235 is Float.MAX_VALUE without power.
+      max = 3.4028235 * Math.pow(10, magnitude);
+    }
+
     double sign = (Math.random() >= 0.5) ? -1.0 : 1.0;
-    double absvalue = (Math.random() * max);  
+    double absvalue = (Math.random() * max);
     return new Double(sign * absvalue);
   }
 }
