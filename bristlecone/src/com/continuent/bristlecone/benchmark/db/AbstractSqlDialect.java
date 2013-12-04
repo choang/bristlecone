7cd69c2a30cd03d646b981b3b708e5d7dbfc31b9
stephane.giron <stephane.giron@continuent.com>
2013-12-04 15:26:46 +0000

    Fixing decimal handling to generate valid decimals

--- bristlecone/src/com/continuent/bristlecone/benchmark/db/AbstractSqlDialect.java
+++ bristlecone/src/com/continuent/bristlecone/benchmark/db/AbstractSqlDialect.java
@@ -1,6 +1,6 @@
 /**
  * Bristlecone Test Tools for Databases
- * Copyright (C) 2006-2007 Continuent Inc.
+ * Copyright (C) 2006-2013 Continuent Inc.
  * Contact: bristlecone@lists.forge.continuent.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  *
  * Initial developer(s): Robert Hodges and Ralph Hannus.
- * Contributor(s):
+ * Contributor(s): Stephane Giron
  */
 
 package com.continuent.bristlecone.benchmark.db;
@@ -100,7 +100,8 @@ public abstract class AbstractSqlDialect implements SqlDialect
         {
             sb.append("(").append(col.getLength());
             if (implementationTypeNeedsPrecision(col.getType())
-                    && col.getPrecision() > 0)
+                    && col.getPrecision() > 0
+                    && col.getPrecision() <= col.getLength())
             {
                 sb.append(",").append(col.getPrecision());
             }
--- bristlecone/src/com/continuent/bristlecone/benchmark/db/DataGeneratorForDecimal.java
+++ bristlecone/src/com/continuent/bristlecone/benchmark/db/DataGeneratorForDecimal.java
@@ -1,6 +1,6 @@
 /**
  * Bristlecone Test Tools for Databases
- * Copyright (C) 2006-2009 Continuent Inc.
+ * Copyright (C) 2006-2013 Continuent Inc.
  * Contact: bristlecone@lists.forge.continuent.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,69 +17,73 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  *
  * Initial developer(s): Scott Martin.
- * Contributor(s):
+ * Contributor(s):Stephane Giron
  */
 
 package com.continuent.bristlecone.benchmark.db;
 
 import java.math.BigDecimal;
 import java.lang.Long;
+
 //import org.apache.log4j.Logger;
 
-
 /**
- * Generates decimal values. 
+ * Generates decimal values.
  * 
  * @author scott.martin
- *
  */
 public class DataGeneratorForDecimal implements DataGenerator
 {
-  //private static Logger     logger                  = Logger.getLogger(DataGeneratorForDecimal.class);
-    
-  private static final BigDecimal max = new BigDecimal(Long.MAX_VALUE);
-  private static final BigDecimal one = new BigDecimal(1);
-  private static final BigDecimal ten = new BigDecimal(10);
-  int length = 10;
-  int precision = 0;
- 
-  /** Create a new instance. */
-  DataGeneratorForDecimal(int length, int precision)
-  {
-      this.length = length;
-      this.precision = precision;
-  }
-  
-  /** Generate a representative value */
-  public Object generate()
-  {
-    boolean positive = (Math.random() >= 0.5) ? true : false;
-    BigDecimal rand = new BigDecimal(Math.random());
-    BigDecimal absvalue = max.multiply(rand);
-    BigDecimal left = absvalue.divideToIntegralValue(one);
-    int i = 1;
-    BigDecimal retval = ten;
-   
-    /* Return only the number of digits permitted */
-    left = left.stripTrailingZeros();
-    for (;left.toPlainString().length() > length;)
+    // private static Logger logger =
+    // Logger.getLogger(DataGeneratorForDecimal.class);
+
+    private static final BigDecimal max       = new BigDecimal(Long.MAX_VALUE);
+    private static final BigDecimal one       = new BigDecimal(1);
+    private static final BigDecimal ten       = new BigDecimal(10);
+    int                             length    = 10;
+    int                             precision = 0;
+
+    /** Create a new instance. */
+    DataGeneratorForDecimal(int length, int precision)
     {
-        left = left.divideToIntegralValue(ten);
-        left = left.stripTrailingZeros();
-    }
-    absvalue = left;
-    
-    /* Shift decimal random number of places */
-    int placesRight = (int)(Math.random() * (double)(length + 1));
-    for (i = 0; i < placesRight; i++)
-    {
-        absvalue = absvalue.divide(ten);
-        absvalue = absvalue.stripTrailingZeros();
+        this.length = length;
+        if (precision <= length)
+            this.precision = precision;
     }
 
-    /* half the time return negative values */
-    if (positive) retval = absvalue;
-    else retval = absvalue.negate();
-    return retval;
-  }
+    /** Generate a representative value */
+    public Object generate()
+    {
+        boolean positive = (Math.random() >= 0.5) ? true : false;
+        BigDecimal rand = new BigDecimal(Math.random());
+        BigDecimal absvalue = max.multiply(rand);
+        BigDecimal left = absvalue.divideToIntegralValue(one);
+        int i = 1;
+        BigDecimal retval = ten;
+
+        /* Return only the number of digits permitted */
+        left = left.stripTrailingZeros();
+        for (; left.toPlainString().length() > length;)
+        {
+            left = left.divideToIntegralValue(ten);
+            left = left.stripTrailingZeros();
+        }
+        absvalue = left;
+
+        /* Shift decimal to match precision, in order to avoid truncation issues */
+        int placesRight = precision;
+        // (int)(Math.random() * (double)(length + 1));
+        for (i = 0; i < placesRight; i++)
+        {
+            absvalue = absvalue.divide(ten);
+            absvalue = absvalue.stripTrailingZeros();
+        }
+
+        /* half the time return negative values */
+        if (positive)
+            retval = absvalue;
+        else
+            retval = absvalue.negate();
+        return retval;
+    }
 }
